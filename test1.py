9cb21da3fe1c6548f1f06c929bfdda38f748784c
Brian Navaluna <b.e.navaluna@gmail.com>
2016-12-15 17:05:44 -0800

    minor changes added

--- test1.py
+++ test1.py
@@ -2,3 +2,7 @@
 
 print("success for test1")
 
+series = {'rate1':'good', 'rate2':'better', 'rate3':'best'}
+
+
+
--- test2.py
+++ test2.py
@@ -3,3 +3,6 @@
 print("success for test2")
 
 some_things = ['demo', 'post']
+
+def makeIt():
+    print("makeIt works!")
