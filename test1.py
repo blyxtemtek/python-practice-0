1f5c4c8cfd61c78d671cb1c3e4f3b26773189ea4
Brian Navaluna <b.e.navaluna@gmail.com>
2017-01-20 15:00:23 -0800

    more tests added

--- test1.py
+++ test1.py
@@ -7,4 +7,12 @@ name = 'sample1'
 series = {'rate1':'good', 'rate2':'better', 'rate3':'best'}
 
 
+#first 50 numbers (excluding 50)
+for i in range(1,50):
+  #use modulus to check result is != 0
+  if (i % 2 != 0):
+    #print odds
+    print(i)
+
+
 
