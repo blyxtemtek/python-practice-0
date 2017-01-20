0d56167477b702ce3431cddc840d2412808c8170
Brian Navaluna <b.e.navaluna@gmail.com>
2017-01-20 14:57:24 -0800

    added more code

--- test3.py
+++ test3.py
@@ -3,3 +3,17 @@
 def newTopic:
     print("test3...")
     sample = input("what is your type?")
+
+while True:
+  
+  try:
+    number = float(input("please enter decimal: "))
+    break
+  #value error handler
+  except ValueError:
+    print("you didn't enter a decimal")
+  #unknown error handler  
+  except: 
+    print("unknown error occured")
+
+print("thank you for entering a decimal")
